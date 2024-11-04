d3fd4f6f5c03cd29d967a303449b4b4b594a110a
Srijita3103 <srijitahmz@gmail.com>
2024-11-04 18:27:53 +0530

    Update TaskList.js

--- task-manager/src/Components/TaskList.js
+++ task-manager/src/Components/TaskList.js
@@ -1,1 +1,20 @@
+import React from "react";
+import TaskItem from "./TaskItem";
+
+const TaskList = ({ tasks, onUpdateTask, onDeleteTask, onEditTask }) => (
+  <div className="mt-3">
+    <h2>Tasks</h2>
+    {tasks.map((task) => (
+      <TaskItem
+        key={task.id}
+        task={task}
+        onUpdateTask={onUpdateTask}
+        onDeleteTask={onDeleteTask}
+        onEditTask={onEditTask}
+      />
+    ))}
+  </div>
+);
+
+export default TaskList;
 
